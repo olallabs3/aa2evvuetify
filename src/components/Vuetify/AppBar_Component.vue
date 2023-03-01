d43e4d0c299eed5bb6b50ce8998d800ab57f5bad
GonzaloGon <a25278@svalero.com>
2023-03-01 21:07:10 +0100

    Vue va bien

---
+++ src/components/Vuetify/AppBar_Component.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <v-app-bar
+      height="100px"
+      color="red lighten-1 text-center"
+      dense
+      dark
+    >
+      <!--<v-app-bar-nav-icon></v-app-bar-nav-icon>-->
+      <v-img :src="require('../assets/1000comics.png')"
+        contain
+        height="80"
+        position="0"
+        max-width="fit-content"
+      />
+      
+      <v-toolbar
+        elevation="0"
+        color="red lighten-1 text-center"
+      >
+        <v-toolbar-item>Inicio</v-toolbar-item>
+        <v-spacer></v-spacer>
+        <v-toolbar-item>Comics</v-toolbar-item>
+        <v-spacer></v-spacer>
+        <v-toolbar-item>Figuras</v-toolbar-item>
+        <v-spacer></v-spacer>
+        <v-toolbar-item>Merchandising</v-toolbar-item>
+        <v-spacer></v-spacer>
+        <v-toolbar-item>Juegos</v-toolbar-item>
+        <v-spacer></v-spacer>
+        <v-toolbar-item>Noticias</v-toolbar-item>
+      <!--<v-toolbar-title>Título soso</v-toolbar-title>-->
+      </v-toolbar>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+  </div>
+</template>
---
+++ src/components/Vuetify/Card_Component.vue
@@ -0,0 +1,100 @@
+<template>
+    <v-container class="bg-surface-variant">
+        <v-row no-gutters>
+            <v-col
+                v-for="(libro) in catalogo"
+                :key="libro.titulo"
+
+                cols="12"
+                sm="3"
+            >
+            <v-sheet class="pa-2 ma-2">
+                <v-card
+                class="mx-auto"
+                max-width="200"
+                min-height="525"
+                >
+                    <v-img :src="require(`../assets/${libro.imagen}`)"
+                        cover
+                    />
+
+                    <v-btn
+                    block
+                    color="green">
+                        EN STOCK
+                        <v-icon>mdi-cart</v-icon>
+                    </v-btn>
+                    <div>
+                        <v-card-title>{{ libro.titulo }}</v-card-title>
+                        <v-card-subtitle>{{ libro.descripcion }}</v-card-subtitle>
+                        <v-card-text>{{ libro.precio }}€</v-card-text>
+                    </div>
+                    
+                </v-card>
+            </v-sheet>
+            </v-col>
+
+        </v-row>
+    </v-container>
+
+<!--     <v-card
+    class="mx-auto"
+    max-width="200"
+    >
+        <v-img :src="require('../assets/Marin.jpg')"
+            cover
+        />
+
+        <v-btn
+        block
+        color="green">
+            EN STOCK
+            <v-icon>mdi-cart</v-icon>
+        </v-btn>
+
+        <v-card-title>Sexy Cosplay Doll 01</v-card-title>
+
+        <v-card-subtitle>Manga, Panini, Sexy Cosplay Doll 01</v-card-subtitle>
+        <v-card-text>8,08</v-card-text>
+    </v-card> -->
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            catalogo:[
+                {imagen: 'Marin.jpg', titulo: 'Sexy Cosplay Doll 01', descripcion: 'Manga, Panini, Sexy Cosplay Doll 01', precio: '8,08'},
+                {imagen: 'mieruko03.jpg', titulo: 'Mieruko Chan Slice of Horror Story 03', descripcion: 'Manga, Ivrea, Mieruko Chan Slice of Horror Story 03', precio: '8,08'},
+                {imagen: 'dan-da-dan.jpg', titulo: 'Dan Da Dan 01+02', descripcion: 'Manga, Norma Editorial, Dan Da Dan 01+02', precio: '7,60'},
+                {imagen: 'chainsaw-man-01.jpg', titulo: 'Chainsaw Man 01', descripcion: 'Manga, Norma Editorial, Chainsaw Man 01', precio: '8,55'},
+            ]
+            
+/*             images:[
+                '..\\assets\\Marin.jpg',
+                '..\\assets\\mieruko03.jpg',
+                '..\\assets\\dan-da-dan.jpg',
+                '..\\assets\\chainsaw-man-01.jpg',
+            ],
+            titulos:[
+                'Sexy Cosplay Doll 01',
+                'Mieruko Chan Slice of Horror Story 03',
+                'Dan Da Dan 01+02',
+                'Chainsaw Man 01',
+            ],
+            descripciones:[
+                'Manga, Panini, Sexy Cosplay Doll 01',
+                'Manga, Ivrea, Mieruko Chan Slice of Horror Story 03',
+                'Manga, Norma Editorial, Dan Da Dan 01+02',
+                'Manga, Norma Editorial, Chainsaw Man 01',
+            ],
+            precios:[
+                '8,08',
+                '8,08',
+                '7,60',
+                '8,55',
+            ], */
+        }
+    }
+}
+</script>
