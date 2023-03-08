c095b5c86d1d07e162f3628ad151765cdab23400
Olalla <a25944@svalero.com>
2023-03-08 15:04:22 +0100

    perfil usuario

--- src/components/Vuetify/PerfilUsuario.vue
+++ src/components/Vuetify/PerfilUsuario.vue
@@ -1,36 +1,135 @@
 <template>
-    <v-container class="grey lighten-5">
-      <v-row>
-        <v-col
-          v-for="n in 16"
-          :key="n"
-          cols="4"
-          sm="2"
-        >
-          <v-card
-            class="pa-2"
-            outlined
-            tile
-          >
-            <v-img
-              :src="PorDecidirTodavía"
-              aspect-ratio="1"
-              cover>
-            </v-img>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
+  <v-container class="grey lighten-5 tablaG">
+    <div>
+    <v-img lazy-src="https://picsum.photos/id/11/10/6" max-height="150" max-width="250"
+      src="https://picsum.photos/id/11/500/300"></v-img>
+      <p class="nombre">Nombre de la cuenta</p>
+      <!-- No se si queremos editar cuenta usuario -->
+      <v-btn class="nombre">Editar cuenta</v-btn>
+    </div>
+    <v-spacer></v-spacer>
+    <v-simple-table class="tabla">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-center">
+              Nombre usuario
+            </th>
+            <th class="text-center">
+              Fecha creación usuario
+            </th>
+            <th class="text-center">
+              Acciones
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in users" :key="item.nombre">
+            <td class="text-center">{{ item.nombre }}</td>
+            <td class="text-center">{{ item.fecha }}</td>
+            <v-btn class="btneliminar">Eliminar</v-btn>
+          </tr>
+          
+        </tbody>
+      </template>
+    </v-simple-table>
+    <v-spacer></v-spacer>
+    <v-simple-table class="tabla">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-center">
+              Nombre Videojuego
+            </th>
+            <th class="text-left">
+              Unidades restantes
+            </th>
+            <th class="text-left">
+              Acciones
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in videojuegos" :key="item.nombre">
+            <td>{{ item.nombre }}</td>
+            <td>{{ item.unidades }}</td>
+            <v-btn class="btneliminar">Eliminar</v-btn>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+
+  </v-container>
+</template>
   
-  <script>
-  export default {
-      data(){
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        {
+          nombre: 'Usuario1',
+          fecha: '12/03/1986',
+        },
+        {
+          nombre: 'Usuario1',
+          fecha: '12/03/1986',
+        },
+        {
+          nombre: 'Usuario1',
+          fecha: '12/03/1986',
+        },
+        {
+          nombre: 'Usuario1',
+          fecha: '12/03/1986',
+        },
+        {
+          nombre: 'Usuario1',
+          fecha: '12/03/1986',
+        },
+      ],
+      videojuegos: [
+        {
+          nombre: 'Videojuego1',
+          unidades: 2,
+        },
+        {
+          nombre: 'Videojuego2',
+          unidades: 3,
+        },
+        {
+          nombre: 'Videojuego3',
+          unidades: 1,
+        },
+        {
+          nombre: 'Videojuego4',
+          unidades: 2,
+        },
+        {
+          nombre: 'Videojuego5',
+          unidades: 0,
+        },
+      ],
+    }
+  },
+}
+</script>
   
-      }
-  }
-  </script>
-  
-  <style>
-  
-  </style>
+<style>
+.tabla {
+  width: 350px;
+}
+
+.btneliminar{
+  width: 40px;
+  height: 40px;
+  font-size: 12px;
+}
+
+.tablaG {
+  display: flex;
+}
+.nombre{
+  margin-left: 15%;
+}
+</style>
